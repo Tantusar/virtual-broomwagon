9ee6d7ee3d818ed05a66c131d15da4928bbc66cd
Thomas Chick <Tantusar@users.noreply.github.com>
2023-05-07 20:49:49 +1000

    big dope! big dope! big dope!

--- generate.py
+++ generate.py
@@ -181,4 +181,4 @@ for item in iglob('**/*.json', recursive=True):
                 outer.text = timeformat((waypoint[1]/speed)*60)
                 ET.SubElement(outer, "br").tail = f"+ {timeformat(converter(speed,replacements['STAGE_COEFFICIENT_LIST'], data['length'], current['coefficients'][0]) * (waypoint[1]/data['length']), False)}"
 
-        working.write('output/' + item.replace('.json', f'/{c}.html'), method="html", doctype="<!DOCTYPE html>")
+        ET.write(working, 'output/' + item.replace('.json', f'/{c}.html'), method="html", doctype="<!DOCTYPE html>")
